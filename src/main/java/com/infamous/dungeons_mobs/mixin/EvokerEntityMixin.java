ed9ff1d6f83fcbf31474684e27d2896891dc5018
people04 <103363415+people04@users.noreply.github.com>
2022-06-09 20:05:44 +0800

    Update EvokerEntityMixin.java

--- src/main/java/com/infamous/dungeons_mobs/mixin/EvokerEntityMixin.java
+++ src/main/java/com/infamous/dungeons_mobs/mixin/EvokerEntityMixin.java
@@ -1,18 +1,27 @@
 package com.infamous.dungeons_mobs.mixin;
 
+import com.infamous.dungeons_mobs.entities.illagers.DungeonsVindicatorEntity;
 import com.infamous.dungeons_mobs.entities.illagers.minibosses.DungeonsEvokerEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ILivingEntityData;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.merchant.villager.AbstractVillagerEntity;
+import net.minecraft.entity.monster.AbstractIllagerEntity;
+import net.minecraft.entity.monster.AbstractRaiderEntity;
 import net.minecraft.entity.monster.EvokerEntity;
 import net.minecraft.entity.monster.SpellcastingIllagerEntity;
+import net.minecraft.entity.passive.IronGolemEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.IServerWorld;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
 
 import javax.annotation.Nullable;
@@ -24,40 +33,30 @@ public abstract class EvokerEntityMixin extends SpellcastingIllagerEntity {
         super(p_i48556_1_, p_i48556_2_);
     }
 
+    public void s(){
+        if (this.getCurrentRaid() != null) {
+
+                EvokerEntityMixin v = this;
+                CompoundNBT v1 = new CompoundNBT();
+                v1 = v.saveWithoutId(v1);
+                BlockPos vvf = v.blockPosition();
+                Biome b = this.level.getBiome(blockPosition());
+                ResourceLocation bb = b.getRegistryName();
+                DungeonsEvokerEntity vv = new DungeonsEvokerEntity(this.level);
+                vv.moveTo(vvf, 0F, 0F);
+                vv.load(v1);
+                vv.addEffect(new EffectInstance(Effects.HEAL, 10, 6, (false), (false)));
+                v.level.addFreshEntity(vv);
+
+                v.remove();
+            }
+    }
+
     @Nullable
     @Override
     public ILivingEntityData finalizeSpawn(IServerWorld p_213386_1_, DifficultyInstance p_213386_2_, SpawnReason p_213386_3_, @Nullable ILivingEntityData p_213386_4_, @Nullable CompoundNBT p_213386_5_) {
-        ILivingEntityData spawnData = super.finalizeSpawn(p_213386_1_, p_213386_2_, p_213386_3_, p_213386_4_, p_213386_5_);
-
-        EvokerEntityMixin v = this;
-        CompoundNBT v1 = new CompoundNBT();
-        v1 = v.saveWithoutId(v1);
-        BlockPos vvf = v.blockPosition();
-        DungeonsEvokerEntity vv = new DungeonsEvokerEntity(this.level);
-        vv.moveTo(vvf, 0F, 0F);
-        vv.load(v1);
-        vv.addEffect(new EffectInstance(Effects.HEAL, 10, 6, (false), (false)));
-        v.level.addFreshEntity(vv);
-
-        v.remove();
-
-        return spawnData;
+        this.s();
+        return super.finalizeSpawn(p_213386_1_, p_213386_2_, p_213386_3_, p_213386_4_, p_213386_5_);
     }
 
-    @Override
-    public void remove() {
-        if (this.getCurrentRaid() == null){
-            EvokerEntityMixin v = this;
-            CompoundNBT v1 = new CompoundNBT();
-            v1 = v.saveWithoutId(v1);
-            BlockPos vvf = v.blockPosition();
-            DungeonsEvokerEntity vv = new DungeonsEvokerEntity(this.level);
-            vv.moveTo(vvf, 0F, 0F);
-            vv.load(v1);
-            vv.addEffect(new EffectInstance(Effects.HEAL, 10, 6, (false), (false)));
-            v.level.addFreshEntity(vv);
-        }
-
-        super.remove();
-    }
 }
