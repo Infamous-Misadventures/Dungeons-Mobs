99119a810f58634557479495f10dd92d4695c27b
people04 <103363415+people04@users.noreply.github.com>
2022-06-12 19:06:18 +0800

    Add files via upload

--- src/main/java/com/infamous/dungeons_mobs/client/models/summonables/IceCloudModel.java
+++ src/main/java/com/infamous/dungeons_mobs/client/models/summonables/IceCloudModel.java
@@ -3,41 +3,59 @@ package com.infamous.dungeons_mobs.client.models.summonables;// Made with Blockb
 // Paste this class into your mod and generate all required imports
 
 
-import com.infamous.dungeons_mobs.entities.summonables.IceCloudEntity;
-import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.vertex.IVertexBuilder;
-import net.minecraft.client.renderer.entity.model.EntityModel;
-import net.minecraft.client.renderer.model.ModelRenderer;
-import net.minecraft.entity.Entity;
+import com.infamous.dungeons_mobs.DungeonsMobs;
+import com.infamous.dungeons_mobs.entities.summonables.ConstructEntity;
+import com.infamous.dungeons_mobs.entities.summonables.GeomancerBombEntity;
+import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
 
-public class IceCloudModel<T extends IceCloudEntity> extends EntityModel<Entity> {
-	private final ModelRenderer icecloud;
-
-	public IceCloudModel() {
-		texWidth = 64;
-		texHeight = 64;
-
-		icecloud = new ModelRenderer(this);
-		icecloud.setPos(0.0F, 24.0F, 0.0F);
-		icecloud.texOffs(0, 0).addBox(0.0F, -16.0F + 1.0F, -16.0F, 16.0F, 16.0F, 16.0F, 0.0F, false);
-		icecloud.texOffs(0, 0).addBox(-16.0F, -16.0F, -16.0F, 16.0F, 16.0F, 16.0F, 0.0F, false);
-		icecloud.texOffs(0, 0).addBox(-16.0F, -16.0F - 2.0F, 0.0F, 16.0F, 16.0F, 16.0F, 0.0F, false);
-		icecloud.texOffs(0, 0).addBox(0.0F, -16.0F - 1.0F, 0.0F, 16.0F, 16.0F, 16.0F, 0.0F, false);
+public class IceCloudModel extends AnimatedGeoModel {
+	   
+	@Override
+	public ResourceLocation getAnimationFileLocation(Object entity) {
+		return new ResourceLocation(DungeonsMobs.MODID, "animations/ice_cloud.animation.json");
 	}
 
 	@Override
-	public void setupAnim(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch){
-		//previously the render function, render code was moved to a method below
+	public ResourceLocation getModelLocation(Object entity) {
+		return new ResourceLocation(DungeonsMobs.MODID, "geo/ice_cloud.geo.json");
 	}
 
 	@Override
-	public void renderToBuffer(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
-		icecloud.render(matrixStack, buffer, packedLight, packedOverlay);
+	public ResourceLocation getTextureLocation(Object entity) {
+		//ChorusGormandizerEntity entityIn = (ChorusGormandizerEntity) entity;
+		return new ResourceLocation(DungeonsMobs.MODID, "textures/entity/illager/iceologer.png");
 	}
 
-	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
-		modelRenderer.xRot = x;
-		modelRenderer.yRot = y;
-		modelRenderer.zRot = z;
+
+	/*public IBone getArm(HandSide p_191216_1_) {
+		return this.getAnimationProcessor().getBone("rightArm");
 	}
+
+
+	public void translateToHand(HandSide p_225599_1_, MatrixStack p_225599_2_) {
+		this.translateAndRotate(this.getAnimationProcessor().getBone("body"), p_225599_2_, 0.0, 0.0, 0.0);
+		this.translateAndRotate(this.getAnimationProcessor().getBone("rightArm"), p_225599_2_, 0.0, 0.0, 0.0);
+	}
+
+	public void translateAndRotate(IBone bone, MatrixStack p_228307_1_, double moveX, double moveY, double moveZ) {
+
+		if (bone.getRotationZ() != 0.0F) {
+			p_228307_1_.mulPose(Vector3f.ZP.rotation(bone.getRotationZ()));
+		}
+
+		if (bone.getRotationY() != 0.0F) {
+			p_228307_1_.mulPose(Vector3f.YP.rotation(bone.getRotationY()));
+		}
+
+		if (bone.getRotationX() != 0.0F) {
+			p_228307_1_.mulPose(Vector3f.XP.rotation(bone.getRotationX()));
+		}
+		
+		p_228307_1_.translate((double)((bone.getPivotX() + moveX) / 16.0F), (double)((bone.getPivotY() + moveY) / 16.0F), (double)((bone.getPivotZ() + moveZ) / 16.0F));
+   
+      
+	}*/
 }
