9b61741d7d09b44522c1e672e222791109923a00
people04 <103363415+people04@users.noreply.github.com>
2022-06-12 18:53:00 +0800

    Update SensorMapModifier.java

--- src/main/java/com/infamous/dungeons_mobs/worldgen/SensorMapModifier.java
+++ src/main/java/com/infamous/dungeons_mobs/worldgen/SensorMapModifier.java
@@ -32,6 +32,7 @@ public class SensorMapModifier {
         villagerHostiles.put(ModEntityTypes.MOUNTAINEER.get(), 10.0F);
         villagerHostiles.put(ModEntityTypes.ARMORED_MOUNTAINEER.get(), 10.0F);
 
+        villagerHostiles.put(ModEntityTypes.ZOMBIE.get(), 8.0F);
         villagerHostiles.put(ModEntityTypes.ARMORED_ZOMBIE.get(), 8.0F);
         villagerHostiles.put(ModEntityTypes.FROZEN_ZOMBIE.get(), 8.0F);
         villagerHostiles.put(ModEntityTypes.JUNGLE_ZOMBIE.get(), 8.0F);
