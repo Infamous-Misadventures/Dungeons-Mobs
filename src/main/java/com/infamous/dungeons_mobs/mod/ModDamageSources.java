fda88ec028f8dffd001adee7bd416333f8c9d6aa
Alexander's Fun And Games <89595306+AlexandersFunAndGames@users.noreply.github.com>
2022-08-25 15:16:56 +0100

    DamageSource and a new goal
    Added Ice Chunk DamageSource and ApproachTargetGoal, which is used by the Iceologer to keep at a somewhat close distance to its target

--- src/main/java/com/infamous/dungeons_mobs/mod/ModDamageSources.java
+++ src/main/java/com/infamous/dungeons_mobs/mod/ModDamageSources.java
@@ -3,6 +3,7 @@ package com.infamous.dungeons_mobs.mod;
 import java.util.Random;
 
 import com.infamous.dungeons_mobs.entities.projectiles.BlastlingBulletEntity;
+import com.infamous.dungeons_mobs.entities.summonables.IceCloudEntity;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.util.DamageSource;
@@ -14,5 +15,9 @@ public class ModDamageSources {
 
 	public static DamageSource blastlingBullet(BlastlingBulletEntity p_233549_0_, Entity p_233549_1_) {
 		return (new IndirectEntityDamageSource("blastling", p_233549_0_, p_233549_1_)).setProjectile().setExplosion();
+	}
+	
+	public static DamageSource iceChunk(IceCloudEntity p_233549_0_, Entity p_233549_1_) {
+		return (new IndirectEntityDamageSource("ice_chunk", p_233549_0_, p_233549_1_)).setProjectile().setExplosion();
 	}
 }
