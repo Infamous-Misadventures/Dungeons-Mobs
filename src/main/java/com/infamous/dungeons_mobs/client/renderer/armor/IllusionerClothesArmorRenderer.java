087feaa63cc8223fb111942b2c59b42327193ad6
e i am a pikachu <103363415+people04@users.noreply.github.com>
2022-08-26 17:26:33 +0800

    Add files via upload

---
+++ src/main/java/com/infamous/dungeons_mobs/client/renderer/armor/IllusionerClothesArmorRenderer.java
@@ -0,0 +1,11 @@
+package com.infamous.dungeons_mobs.client.renderer.armor;
+
+import com.infamous.dungeons_mobs.client.models.armor.IllusionerClothesModel;
+
+public class IllusionerClothesArmorRenderer extends BaseDungeonsGeoArmorRenderer {
+    public IllusionerClothesArmorRenderer()
+    {
+        super(new IllusionerClothesModel());
+    }
+
+}
