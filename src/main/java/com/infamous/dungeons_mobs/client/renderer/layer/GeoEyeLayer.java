a379ea9e1e99401be1fc737fa37ef52aa7ba84f5
Alexander's Fun And Games <89595306+AlexandersFunAndGames@users.noreply.github.com>
2022-08-30 10:11:29 +0100

    Redstone Golem pulses
    Now the Redstone Golem's redstone slowly pulses

--- src/main/java/com/infamous/dungeons_mobs/client/renderer/layer/GeoEyeLayer.java
+++ src/main/java/com/infamous/dungeons_mobs/client/renderer/layer/GeoEyeLayer.java
@@ -4,6 +4,7 @@ import java.util.function.BiFunction;
 
 import com.infamous.dungeons_mobs.DungeonsMobs;
 import com.infamous.dungeons_mobs.entities.ender.BlastlingEntity;
+import com.infamous.dungeons_mobs.entities.redstone.RedstoneGolemEntity;
 import com.infamous.dungeons_mobs.entities.summonables.GeomancerBombEntity;
 import com.mojang.blaze3d.matrix.MatrixStack;
 
@@ -13,6 +14,7 @@ import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Util;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import software.bernie.geckolib3.core.IAnimatable;
@@ -36,13 +38,12 @@ public class GeoEyeLayer<T extends LivingEntity & IAnimatable> extends GeoLayerR
 				float ageInTicks, float netHeadYaw, float headPitch) {
 			
 		      GeoModelProvider<T> geomodel = (GeoModelProvider<T>)this.getEntityModel();
-				//IGeoRenderer<LivingEntity> renderer = (IGeoRenderer<LivingEntity>) AnimationUtils.getRenderer(entitylivingbaseIn);
 		      if (entitylivingbaseIn instanceof GeomancerBombEntity && ((GeomancerBombEntity)entitylivingbaseIn).getLifeTicks() < 60 && ((GeomancerBombEntity)entitylivingbaseIn).getLifeTicks() >= 30) {
 			      renderModel(geomodel, new ResourceLocation(DungeonsMobs.MODID, "textures/entity/constructs/geomancer_bomb_eyes_1.png"), matrixStackIn, bufferIn, packedLightIn, entitylivingbaseIn, 1.0F, 1.0F, 1.0F, 1.0F);  
 		      } else if (entitylivingbaseIn instanceof GeomancerBombEntity && ((GeomancerBombEntity)entitylivingbaseIn).getLifeTicks() < 30 && ((GeomancerBombEntity)entitylivingbaseIn).getLifeTicks() >= 0) {
 			      renderModel(geomodel, new ResourceLocation(DungeonsMobs.MODID, "textures/entity/constructs/geomancer_bomb_eyes_2.png"), matrixStackIn, bufferIn, packedLightIn, entitylivingbaseIn, 1.0F, 1.0F, 1.0F, 1.0F); 
 		      } else if (entitylivingbaseIn instanceof GeomancerBombEntity) {
- 
+		    	  
 		      } else if (entitylivingbaseIn instanceof BlastlingEntity) {
 			      renderModel(geomodel, new ResourceLocation(DungeonsMobs.MODID, "textures/entity/ender/blastling" + ( 1 + ((int)((BlastlingEntity)entitylivingbaseIn).flameTicks) % 3) + "_eyes.png"), matrixStackIn, bufferIn, packedLightIn, entitylivingbaseIn, 1.0F, 1.0F, 1.0F, 1.0F);
 		      } else {
