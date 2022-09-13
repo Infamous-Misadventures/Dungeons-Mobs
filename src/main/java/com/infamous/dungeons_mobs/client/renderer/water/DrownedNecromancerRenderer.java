35ee561e4bbb981d213891e039ecce8671cad59b
e i am a pikachu <103363415+people04@users.noreply.github.com>
2022-09-13 21:45:52 +0800

    Add files via upload

--- src/main/java/com/infamous/dungeons_mobs/client/renderer/water/DrownedNecromancerRenderer.java
+++ src/main/java/com/infamous/dungeons_mobs/client/renderer/water/DrownedNecromancerRenderer.java
@@ -3,53 +3,35 @@ package com.infamous.dungeons_mobs.client.renderer.water;
 import com.infamous.dungeons_mobs.client.models.ocean.DrownedNecromancerModel;
 import com.infamous.dungeons_mobs.entities.water.DrownedNecromancerEntity;
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.entity.BipedRenderer;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
-import net.minecraft.client.renderer.entity.layers.BipedArmorLayer;
-import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.vector.Vector3f;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
-import static com.infamous.dungeons_mobs.DungeonsMobs.MODID;
+public class DrownedNecromancerRenderer extends GeoEntityRenderer<DrownedNecromancerEntity> {
 
-@OnlyIn(Dist.CLIENT)
-public class DrownedNecromancerRenderer extends BipedRenderer<DrownedNecromancerEntity, DrownedNecromancerModel<DrownedNecromancerEntity>> {
-    private static final ResourceLocation DROWNED_NECROMANCER_TEXTURE = new ResourceLocation(MODID, "textures/entity/ocean/drowned_necromancer.png");
+    public DrownedNecromancerRenderer(EntityRendererManager renderManager) {
+        super(renderManager, new DrownedNecromancerModel());
+        //this.addLayer(new GeoEyeLayer<>(this, new ResourceLocation(DungeonsMobs.MODID, "textures/entity/enchanter/enchanter_eyes.png")));
+        //this.addLayer(new GeoHeldItemLayer<>(this, 0.0, 0.0, 0.5));
 
-    public DrownedNecromancerRenderer(EntityRendererManager renderManagerIn) {
-        super(renderManagerIn, new DrownedNecromancerModel<>(), 0.5F);
-        this.addLayer(new BipedArmorLayer<>(this, new BipedModel<>(0.5F), new BipedModel<>(1.0F)));
     }
 
-    @Override
-    public void render(DrownedNecromancerEntity p_225623_1_, float p_225623_2_, float p_225623_3_, MatrixStack p_225623_4_, IRenderTypeBuffer p_225623_5_, int p_225623_6_) {
-        super.render(p_225623_1_, p_225623_2_, p_225623_3_, p_225623_4_, p_225623_5_, p_225623_6_);
-    }
-
-    @Override
-    protected void scale(DrownedNecromancerEntity necromancerEntity, MatrixStack matrixStack, float v) {
+    protected void applyRotations(DrownedNecromancerEntity entityLiving, MatrixStack matrixStackIn, float ageInTicks,
+                                  float rotationYaw, float partialTicks) {
         float scaleFactor = 1.2F;
-        matrixStack.scale(scaleFactor, scaleFactor, scaleFactor);
-        super.scale(necromancerEntity, matrixStack, v);
-    }
-
-    protected void setupRotations(DrownedNecromancerEntity drownedNecromancer, MatrixStack matrixStack, float p_225621_3_, float p_225621_4_, float p_225621_5_) {
-        super.setupRotations(drownedNecromancer, matrixStack, p_225621_3_, p_225621_4_, p_225621_5_);
-        float swimAmount = drownedNecromancer.getSwimAmount(p_225621_5_);
-        if (swimAmount > 0.0F) {
-            matrixStack.mulPose(Vector3f.XP.rotationDegrees(MathHelper.lerp(swimAmount, drownedNecromancer.xRot, -10.0F - drownedNecromancer.xRot)));
-        }
+        matrixStackIn.scale(scaleFactor, scaleFactor, scaleFactor);
+        super.applyRotations(entityLiving, matrixStackIn, ageInTicks, rotationYaw, partialTicks);
 
     }
 
-    /**
-     * Returns the location of an entity's texture.
-     */
-    public ResourceLocation getTextureLocation(DrownedNecromancerEntity entity) {
-        return DROWNED_NECROMANCER_TEXTURE;
+    @Override
+    public RenderType getRenderType(DrownedNecromancerEntity animatable, float partialTicks, MatrixStack stack,
+                                    IRenderTypeBuffer renderTypeBuffer, IVertexBuilder vertexBuilder, int packedLightIn,
+                                    ResourceLocation textureLocation) {
+        return RenderType.entityTranslucent(getTextureLocation(animatable));
     }
+
 }
