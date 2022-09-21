a770d4af8a7c9967557dc434ec91b7092444f665
Patrigan <patrick.provinciael@gmail.com>
2022-09-22 00:00:24 +0200

    Move to Enchant With Mob. Remove old MobEnchantments.

--- src/main/java/com/infamous/dungeons_mobs/mobenchants/BurningMobEnchant.java
+++ src/main/java/com/infamous/dungeons_mobs/mobenchants/BurningMobEnchant.java
@@ -1,6 +1,6 @@
-package com.infamous.dungeons_mobs.mobenchantments;
+package com.infamous.dungeons_mobs.mobenchants;
 
-import com.infamous.dungeons_libraries.mobenchantments.MobEnchantment;
+import com.baguchan.enchantwithmob.mobenchant.MobEnchant;
 import com.infamous.dungeons_mobs.DungeonsMobs;
 import com.infamous.dungeons_mobs.capabilities.properties.IMobProps;
 import com.infamous.dungeons_mobs.capabilities.properties.MobPropsHelper;
@@ -11,31 +11,32 @@ import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import static com.infamous.dungeons_libraries.mobenchantments.MobEnchantmentHelper.executeIfPresent;
 import static com.infamous.dungeons_libraries.utils.AreaOfEffectHelper.applyToNearbyEntities;
 import static com.infamous.dungeons_libraries.utils.AreaOfEffectHelper.getCanApplyToEnemyPredicate;
 import static com.infamous.dungeons_mobs.DungeonsMobs.PROXY;
-import static com.infamous.dungeons_mobs.mod.ModMobEnchantments.BURNING;
+import static com.infamous.dungeons_mobs.mobenchants.NewMobEnchantUtils.executeIfPresentWithLevel;
+import static com.infamous.dungeons_mobs.mod.ModMobEnchants.BURNING;
 
 @Mod.EventBusSubscriber(modid = DungeonsMobs.MODID)
-public class BurningMobEnchantment extends MobEnchantment {
+public class BurningMobEnchant extends MobEnchant {
 
-    public BurningMobEnchantment(Rarity rarity) {
-        super(rarity);
+    public BurningMobEnchant(Properties properties) {
+        super(properties);
     }
 
     @SubscribeEvent
     public static void OnLivingUpdate(LivingEvent.LivingUpdateEvent event) {
         LivingEntity entity = (LivingEntity) event.getEntity();
 
-        executeIfPresent(entity, BURNING.get(), () -> {
+        executeIfPresentWithLevel(entity, BURNING.get(), (level) -> {
             IMobProps comboCap = MobPropsHelper.getMobPropsCapability(entity);
             if(comboCap == null) return;
             int burnNearbyTimer = comboCap.getBurnNearbyTimer();
             if(burnNearbyTimer <= 0){
+                PROXY.spawnParticles(entity, ParticleTypes.FLAME);
                 applyToNearbyEntities(entity, 1.5F,
                         getCanApplyToEnemyPredicate(entity), (LivingEntity nearbyEntity) -> {
-                            nearbyEntity.hurt(DamageSource.ON_FIRE, 1F);
+                            nearbyEntity.hurt(DamageSource.ON_FIRE, 0.5F * level);
                             PROXY.spawnParticles(nearbyEntity, ParticleTypes.FLAME);
                         }
                 );
--- src/main/java/com/infamous/dungeons_mobs/mod/ModBlocks.java
+++ src/main/java/com/infamous/dungeons_mobs/mod/ModBlocks.java
@@ -1,10 +1,6 @@
 package com.infamous.dungeons_mobs.mod;
 
-import static com.infamous.dungeons_mobs.DungeonsMobs.MODID;
-
-import com.infamous.dungeons_mobs.blocks.CorruptedPyreBlock;
 import com.infamous.dungeons_mobs.blocks.WraithFireBlock;
-
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
@@ -13,6 +9,8 @@ import net.minecraft.block.material.MaterialColor;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+
+import static com.infamous.dungeons_mobs.DungeonsMobs.MODID;
 
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
@@ -23,13 +21,5 @@ public class ModBlocks {
                     .noCollission()
                     .instabreak()
                     .lightLevel((state) -> 10)
-                    .sound(SoundType.WOOL)));
-    
-    // CORRUPTED PYRE
-    public static final RegistryObject<CorruptedPyreBlock> CORRUPTED_PYRE_BLOCK = BLOCKS.register("corrupted_pyre",
-            () -> new CorruptedPyreBlock(AbstractBlock.Properties.of(Material.FIRE, MaterialColor.COLOR_MAGENTA)
-                    .noCollission()
-                    .instabreak()
-                    .lightLevel((state) -> 5)
                     .sound(SoundType.WOOL)));
 }
