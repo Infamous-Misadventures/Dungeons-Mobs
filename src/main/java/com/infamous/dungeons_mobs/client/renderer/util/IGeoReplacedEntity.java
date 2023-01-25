3a02e188f74d78a7c926d047dd925a30639131ab
Patrigan <patrick.provinciael@gmail.com>
2023-01-25 22:25:54 +0100

    Remove outdated code

--- src/main/java/com/infamous/dungeons_mobs/client/renderer/util/IGeoReplacedEntity.java
+++
@@ -1,9 +0,0 @@
-package com.infamous.dungeons_mobs.client.renderer.util;
-
-import net.minecraft.world.entity.Mob;
-
-public interface IGeoReplacedEntity {
-    Mob getMob();
-
-    void setMob(Mob r);
-}
